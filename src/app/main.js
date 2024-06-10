05429c2b05af07055b07fa8ff40a8bc897b6ca6a
Guilherme <Fireguiquenbr@gmail.com>
2024-06-09 22:19:08 -0300

    update

--- src/app/main.js
+++ src/app/main.js
@@ -1,18 +1,42 @@
+const button = document.querySelector('#button');
+
+button.addEventListener('click', function()
+{
+    if (!button.classList.contains('onclic'))
+    {
+        button.classList.add('onclic');
+        setTimeout(function()
+        {
+            button.classList.remove('onclic');
+            button.classList.add('validate');
+        }, 2250);
+    }
+
+            setTimeout(function()
+            {
+                button.classList.remove('validate');
+            }, 5250);
+
+
+})
+
+
+
 $(function() {
   $( "#button" ).click(function() {
-    $( "#button" ).addClass( "onclic", 250, validate);
+    $( "#button" ).addClass( "onclic", 1, validate);
   });
 
   function validate() {
     setTimeout(function() {
       $( "#button" ).removeClass( "onclic" );
-      $( "#button" ).addClass( "validate", 450, callback );
-    }, 2250 );
+      $( "#button" ).addClass( "validate", 1, callback );
+    }, 1 );
   }
     function callback() {
       setTimeout(function() {
         $( "#button" ).removeClass( "validate" );
-      }, 10 );
+      }, 1 );
     }
   });
 
