2146056f72e774e4fdf2bfb9548d242fefa3a39a
Guilherme <Fireguiquenbr@gmail.com>
2024-06-12 21:25:30 -0300

    Update main.js

--- src/app/main.js
+++ src/app/main.js
@@ -2,6 +2,7 @@ const button = document.querySelectorAll("button")[0];
 
 button.addEventListener('click', function()
 {
+
     if (!button.classList.contains('onclic'))
     {
         button.classList.add('onclic');
@@ -12,9 +13,9 @@ button.addEventListener('click', function()
         }, 2250);
     }
 
+
     setTimeout(function()
     {
-        button.style.animation="smooth 5.5s ease-in";
         button.classList.remove('validate');
     }, 5250);
 
